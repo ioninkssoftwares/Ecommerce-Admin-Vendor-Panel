5ba8a83a867aa0cb40138b959f9f5d8f09dc0353
shubham <khobragade.shubham@gmail.com>
2024-05-20 01:03:51 +0530

    added the usename of the vendor at the navbar

--- src/VendorPanel/Components/navbar/VendorNavbar.jsx
+++ src/VendorPanel/Components/navbar/VendorNavbar.jsx
@@ -1,18 +1,23 @@
+import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { CiLogout } from "react-icons/ci";
-import { FaStoreAlt } from "react-icons/fa"
+import { FaStoreAlt, FaUserCircle } from "react-icons/fa"
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useAxios } from "../../../utils/axios";
 
 
 
 
 const VendorNavbar = () => {
     const navigate = useNavigate()
+
     const [cookies, setCookies, removeCookies] = useCookies(['vendorToken']);
+    const [token, setToken] = useState("");
 
     // Todo-localStorageUsed
     const isAdmin = localStorage.getItem("isAdmin");
+
 
     const handleLogout = () => {
         toast("Logout Successfully")
@@ -26,13 +31,77 @@ const VendorNavbar = () => {
     };
 
 
+
+    const [vendorId, setVendorId] = useState('');
+    const [vendorDetails, setVendorDetails] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const instance = useAxios(token);
+
+
+    useEffect(() => {
+        if (cookies && cookies.vendorToken) {
+            console.log(cookies.vendorToken, "fdsfsdfsf")
+            setToken(cookies.vendorToken);
+        }
+    }, [cookies]);
+
+
+    const getVendorId = async () => {
+        setLoading(true)
+        try {
+            const res = await instance.get(
+                `/vendor/getVendorIdByToken`
+            );
+            if (res.data) {
+                console.log(res.data.id, "yuyiyui")
+                setVendorId(res.data.id)
+                setLoading(false);
+            }
+        } catch (e) {
+            setLoading(false);
+            console.log(e)
+            // ErrorDispaly(e);
+        }
+    }
+
+    useEffect(() => {
+        getVendorId()
+    }, [token])
+
+    const getVendorDetails = async () => {
+        setLoading(true)
+        try {
+            const res = await instance.get(
+                `/vendor/vendorByVendorId/${vendorId}`
+            );
+            if (res.data) {
+                console.log(res.data, "sdasdfdf")
+                setLoading(false)
+                setVendorDetails(res.data.data)
+            }
+        } catch (e) {
+            setLoading(false);
+            console.log(e)
+            // ErrorDispaly(e);
+        }
+    }
+
+    useEffect(() => {
+        if (vendorId) {
+            getVendorDetails()
+        }
+    }, [vendorId])
+
+
+
     return (
         <nav className="bg-white">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-end">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* <!-- Mobile menu button--> */}
-                        <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        {/* <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                             <span className="absolute -inset-0.5"></span>
                             <span className="sr-only">Open main menu</span>
 
@@ -43,43 +112,32 @@ const VendorNavbar = () => {
                             <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
-                        </button>
+                        </button> */}
                     </div>
 
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
                         {/* <!-- Profile dropdown --> */}
                         <div className="relative ml-3">
-                            <div className="flex items-center justify-center gap-4">
+                            <div className="flex items-center justify-center gap-2">
 
 
-                                {/* <form className="hidden md:block">
-                                    <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                                    <div className="relative">
-                                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                            </svg>
-                                        </div>
-                                        <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                           
-                                    </div>
-                                </form> */}
+
+                                {/* <button className=" hidden bg-primary-blue px-5 py-2 rounded-lg md:flex items-center justify-center gap-3"><FaStoreAlt />Visit My Store</button> */}
 
 
-                                {/* <button className=" hidden bg-primary-blue px-5 py-2 rounded-lg md:flex items-center justify-center gap-3"><FaStoreAlt />Visit My Store</button>
+                                <div onClick={() => navigate("/vendor/profile")}>
 
+                                    <FaUserCircle className="h-10 w-10 rounded-full text-gray-400 cursor-pointer" />
+                                </div>
 
-                                <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                    <span className="absolute -inset-1.5"></span>
-                                    <span className="sr-only">Open user menu</span>
-                                    <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                </button>
-                                <span className="text-black">Sandeep</span> */}
+                                {vendorDetails && vendorDetails.name && (
+                                    <span onClick={() => navigate("/vendor/profile")} className="text-black cursor-pointer">{vendorDetails.name}</span>
+                                )}
 
-                                {isAdmin ? (<div onClick={handleLogout} style={{ width: "28px", height: "28px", borderRadius: "50%" }}>
+                                {/* {isAdmin ? (<div onClick={handleLogout} style={{ width: "28px", height: "28px", borderRadius: "50%" }}>
                                     <CiLogout className='w-full h-full cursor-pointer hover:text-primary-blue' />
-                                </div>) : ""}
+                                </div>) : ""} */}
 
                             </div>
 
@@ -89,15 +147,15 @@ const VendorNavbar = () => {
             </div>
 
             {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-            <div className="sm:hidden" id="mobile-menu">
+            {/* <div className="sm:hidden" id="mobile-menu">
                 <div className="space-y-1 px-2 pb-3 pt-2">
-                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+
                     <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
                     <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
                     <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
                     <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
                 </div>
-            </div>
+            </div> */}
         </nav>
 
 
